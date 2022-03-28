0ffa93cdcefc69ef6b87a2811f1c6591bab5fd2e
Boxu Fan <boxufan@gmail.com>
2022-03-27 21:44:11 -0500

    change:naming

--- Star-Rating/src/App.js
+++ Star-Rating/src/App.js
@@ -1,20 +1,19 @@
 import './App.css';
-import Star from './assets/star.svg';
 import React, {useState} from 'react';
 
 const RatingSystem = () => {
-	const star = [1, 2, 3, 4, 5];
 	const [rating, setRating] = useState(0);
 
 	return (
 		<div className="App">
 			<h1>5 Star Rating System</h1>
 			<h2>Select a rating: </h2>
-			{[...star].map((star, index) => {
+			{[...Array(5)].map((star, index) => {
 				index += 1;
 				return (
 					<button
-						type="button"
+						type="submit"
+						name="rating"
 						key={index}
 						className={index <= rating ? 'on' : 'off'}
 						onClick={() => setRating(index)}
